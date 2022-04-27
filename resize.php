c28070047d73a1042b15d6f65aaf6941a8ed9057
Daniel Assunção <daniel_cva@msn.com>
2022-04-27 15:45:11 +0100

    Implementing S3 services, to upload images

--- resize.php
+++ resize.php
@@ -1,6 +1,12 @@
 <?php
-    require "vendor/autoload.php";
 
+define('ACCESS_KEY', '');
+define('SECRET_KEY', '');
+define('BUCKET', '');
+
+require "vendor/autoload.php";
+
+use Aws\S3\S3Client;
 use Gumlet\ImageResize;
 
     $width = $_POST['width'];
@@ -23,7 +29,7 @@ use Gumlet\ImageResize;
         die("No File");
 
     //Validate file size
-    $maxBytes = 2048000;
+    $maxBytes = 204800;
     if($file['size'] > $maxBytes)
         die("File size is bigger than $maxBytes bytes");
 
@@ -34,18 +40,43 @@ use Gumlet\ImageResize;
     if(!move_uploaded_file($fileTmp, $path))
         die("Failed to load file");
 
-    //Resize file
-    $image = new ImageResize($path);
-    $image->resizeToHeight($height);
-    $image->resizeToWidth($width);
-    $image->save($filename, IMAGETYPE_WEBP);
+    try {
+        //Resize file
+        $image = new ImageResize($path);
+        $image->resizeToHeight($height);
+        $image->resizeToWidth($width);
+        $image->save($filename, IMAGETYPE_WEBP);
 
-    //Output resized file
-    $image->output(IMAGETYPE_WEBP);
+        //Output resized file
+        // $image->output(IMAGETYPE_WEBP);
+    } catch (\Exception $e)
+    {
+        unset($path);
+        die("Failed to resize file");
+    }
 
-    //Remove file
+    try {       
+        $clientS3 = S3Client::factory(array(
+            'key'    => ACCESS_KEY,
+            'secret' => SECRET_KEY
+        ));
+
+        $response = $clientS3->putObject(array(
+            'Bucket' => BUCKET,
+            'Key'    => "original/$filename",
+            'SourceFile' => $path,
+        ));
+
+        $response = $clientS3->putObject(array(
+            'Bucket' => BUCKET,
+            'Key'    => "resized/$filename",
+            'SourceFile' => $image->__toString(),
+        ));
+
+    } catch(Exception $e) {
+        echo "Erro > {$e->getMessage()}";
+    }
+
+    //Remove temp file
     unlink($path);
-
-    //upload original
-    //upload resized
 ?>
